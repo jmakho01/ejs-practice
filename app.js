6b1ef1de125484b28f749f02977d5c26f376cd3a
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-11-04 11:47:27 -0800

    Part 2 completed

---
+++ app.js
@@ -0,0 +1,22 @@
+// import necessary modules 
+import express from 'express';
+// Initialize express app
+const app = express();
+// Define the port
+const PORT = 3005;
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Home route
+app.get('/', (req, res) => {
+    const person = { name: 'Donald Duck', occupation: 'plumber', favColor: 'blue' };
+    
+    res.render('home', { person });
+});
+
+// additional routes can be added here
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+
